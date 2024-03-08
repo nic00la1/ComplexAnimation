09045b95fc86342b344ef280b86558fe91a9ed7d
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-03-08 21:30:56 +0100

    Initial Commit

---
+++ ComplexAnimation/ComplexAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexAnimationApp.swift
+//  ComplexAnimation
+//
+//  Created by Nicola Kaleta on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ComplexAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
