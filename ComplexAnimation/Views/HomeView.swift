4749eb0a19795c6fc51a8917afb58e9bdca22a0d
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-03-10 08:57:57 +0100

    animation for scrollView books

--- ComplexAnimation/Views/HomeView.swift
+++ ComplexAnimation/Views/HomeView.swift
@@ -37,11 +37,30 @@ struct HomeView: View {
                         ForEach(sampleBooks, id: \.self) {
                             book in
                             BookCardView(book: book)
+                                .onTapGesture {
+                                    withAnimation(.easeIn(duration: 0.15)) {
+                                        animateCurrentBook = true
+                                        selectedBook = book
+                                    }
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+                                        withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
+                                            showDetail = true
+                                        }
+                                    }
+                                }
                         }
                     }
+                    .padding(.bottom, bottomPadding(size: size))
                 }
+                .coordinateSpace(name: "SCROLLVIEW")
             }
         }
+    }
+    
+    func bottomPadding(size: CGSize) -> CGFloat {
+        let cardHeight : CGFloat = 220
+        let scrollHeight : CGFloat = size.height
+        return scrollHeight - cardHeight
     }
     
     @ViewBuilder
@@ -107,6 +126,7 @@ struct HomeView: View {
             }
             .frame(width: size.width)
             .padding(.vertical, 16)
+            .rotation3DEffect(.init(degrees: convertOffsetToRotation(rect: rect)), axis: (x: 1, y: 0, z: 0), anchor: .bottom, anchorZ: 1, perspective: 0.8)
         }
         .frame(height: 220)
         .padding(.horizontal, 16)
@@ -143,6 +163,14 @@ struct HomeView: View {
             }
         }
         .padding(.horizontal, 16)
+    }
+    
+    func convertOffsetToRotation(rect: CGRect) -> CGFloat {
+        let cardHeight = rect.height
+        let minY = rect.minY
+        let progress = minY < 0 ? (minY / cardHeight) : 0
+        let constrainedProgress = min(-progress, 1)
+        return constrainedProgress * 90
     }
 }
 
